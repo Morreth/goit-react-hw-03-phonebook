3b2da69e1dfc1e66b6003bcdcd94b833fcca911e
Olha Brylova <47885094+Morreth@users.noreply.github.com>
2023-02-24 16:45:02 +0200

    src upd

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import { ContactListItem } from '../ContactListItem/ContactListItem';
+import css from './ContactList.module.css';
+
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ removeContact, filteredContacts }) => {
+  return (
+    <ul>
+      {filteredContacts.length > 0 &&
+        filteredContacts.map(({ id, name, number }) => {
+          return (
+            <li className={css.item} key={id}>
+              <ContactListItem
+                name={name}
+                number={number}
+                id={id}
+                removeContact={removeContact}
+              />
+            </li>
+          );
+        })}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+filteredContacts: PropTypes.arrayOf.isRequired,
+id: PropTypes.string.isRequired,
+name: PropTypes.string.isRequired,
+number: PropTypes.string.isRequired,
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import css from './Filter.module.css';
+import PropTypes from 'prop-types';
+
+export const Filter = ({ filter, changeFilter }) => {
+  return (
+    <div className={css.wrapper}>
+      <span>Find contacts by name</span>
+      <input
+        className={css.input}
+        onInput={e => changeFilter(e.target.value)}
+        value={filter}
+        type="text"
+      />
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onInput: PropTypes.node.isRequired,
+};
